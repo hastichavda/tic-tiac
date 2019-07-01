d487f0b892152b309d30d9819eb8460bf7ba6444
hasti chavda <hasiichavda10@gmail.com>
2019-07-01 11:22:05 +0530

    comit

--- tictac.js
+++ tictac.js
@@ -2,7 +2,7 @@ var turn = document.getElementById("turn"),
 // boxes => all boxes
 // X_or_O => to set X or O into the box
    boxes = document.querySelectorAll("#main div"), X_or_O = 0;
-   count = 0;
+  var  count = 0;
 
 function selectWinnerBoxes(b1,b2,b3){
    b1.classList.add("win");
@@ -10,12 +10,10 @@ function selectWinnerBoxes(b1,b2,b3){
    b3.classList.add("win");
    turn.innerHTML = b1.innerHTML + " Won Congrats";
    turn.style.fontSize = "40px";
-   if(count == 8)
-   {
-       alert("tie");
-   }
-   count = 0;
-//    replay();
+
+   count=0;
+   replay();
+
 }
 
 
@@ -36,34 +34,39 @@ function getWinner(){
  if(box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML)
     selectWinnerBoxes(box1,box2,box3);
 
- if(box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML)
+ else if(box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML)
     selectWinnerBoxes(box4,box5,box6);
 
- if(box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML)
+    else if(box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML)
     selectWinnerBoxes(box7,box8,box9);
 
- if(box1.innerHTML !== "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML)
+    else if(box1.innerHTML !== "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML)
     selectWinnerBoxes(box1,box4,box7);
 
- if(box2.innerHTML !== "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML)
+    else  if(box2.innerHTML !== "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML)
     selectWinnerBoxes(box2,box5,box8);
 
- if(box3.innerHTML !== "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML)
+    else if(box3.innerHTML !== "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML)
     selectWinnerBoxes(box3,box6,box9);
 
- if(box1.innerHTML !== "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML)
+    else if(box1.innerHTML !== "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML)
     selectWinnerBoxes(box1,box5,box9);
 
- if(box3.innerHTML !== "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML)
+    else if(box3.innerHTML !== "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML)
     selectWinnerBoxes(box3,box5,box7);
 
-    count++;
+    else if(count > 8){
+       alert("tie");
+       count=0;
+    }
+   
 }
-replay();
+
 
 // set event onclick
 for(var i = 0; i < boxes.length; i++){
    boxes[i].onclick = function(){
+         count++;
        // not allow to change the value of the box
        if(this.innerHTML !== "X" && this.innerHTML !== "O"){
        if(X_or_O%2 === 0){
@@ -81,8 +84,10 @@ for(var i = 0; i < boxes.length; i++){
           X_or_O += 1;  
        }
    }
-       
+
+
    };
+   
 }
 
 function replay(){
