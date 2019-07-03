87b78f8c49c63016ff796e91057a049b67fdc5c4
hasti chavda <hasiichavda10@gmail.com>
2019-07-03 13:08:14 +0530

    commitsecond

--- tictac.js
+++ tictac.js
@@ -3,13 +3,22 @@ var turn = document.getElementById("turn"),
 // X_or_O => to set X or O into the box
    boxes = document.querySelectorAll("#main div"), X_or_O = 0;
   var  count = 0;
-   
-  function dgi(a){
-   return document.getElementById(a);
-  }
-      dgi('p1-name').innerHTML = name;
-		dgi('p2-name').innerHTML = name;
+  var pColor1, pColor2, pName1, pName2;
 
+  var $dropdown1 = $("select[name='dropdown1']");
+var $dropdown2 = $("select[name='dropdown2']");
+
+
+//dropdown disabled
+$dropdown1.change(function() {
+    $dropdown2.find('option').prop("disabled", false);
+    var selectedItem = $(this).val();
+    if (selectedItem) {
+        $dropdown2.find('option[value="' + selectedItem + '"]').prop("disabled", true);
+    }
+});
+
+//select winners
 function selectWinnerBoxes(b1,b2,b3){
    b1.classList.add("win");
    b2.classList.add("win");
@@ -20,7 +29,7 @@ function selectWinnerBoxes(b1,b2,b3){
 }
 replay();
 
-
+//check possibilities
 function getWinner(){
    
    let box1 = document.getElementById("box1"),
@@ -33,11 +42,11 @@ function getWinner(){
        box8 = document.getElementById("box8"),
        box9 = document.getElementById("box9");
 
-// get all posibilites
- if(box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML)
+    // get all posibilites
+    if(box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML)
     selectWinnerBoxes(box1,box2,box3);
 
- else if(box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML)
+    else if(box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML)
     selectWinnerBoxes(box4,box5,box6);
 
     else if(box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML)
@@ -94,6 +103,7 @@ for(var i = 0; i < boxes.length; i++){
    }; 
    
 }
+//startbutton
 
 function startGame(ele) {
    var cont = document.getElementById('main');
@@ -106,7 +116,7 @@ function startGame(ele) {
 }
  
  
-
+//replaybutton
 function replay(){
    var cont = document.getElementById('main');
    if (cont.style.display == 'none') {
@@ -128,3 +138,36 @@ function replay(){
    
 }
 
+//works as document.getelementbyid
+function dgi(a)
+{
+   return document.getElementById(a);
+}
+
+
+function setPlayerName( player, name ){
+    dgi('errBox').innerHTML = '';
+    dgi('startbutton').disabled = false;
+    switch(player){
+        case 1:
+            if( name == pName2 ){
+                dgi('errBox').innerHTML = 'Two players cannot have the same name!';
+                dgi('startbutton').disabled = true;
+                return false;
+            }
+            pName1 = name;
+            return true;
+            break;
+        case 2:
+            if( name == pName1 ){
+                dgi('errBox').innerHTML = 'Two players cannot have the same color!';
+                dgi('startbutton').disabled = true;
+                return false;
+            }
+            pName2 = name;	
+            return true;			
+            break;
+    }
+    dgi('p1-name').innerHTML = name;
+    dgi('p2-name').innerHTML = name;
+}
